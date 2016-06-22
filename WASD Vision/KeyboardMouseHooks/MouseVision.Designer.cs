6606a8e6e8e15dbadb8e2d28f3637f9673e30da2
Todd Stangel <toddbstangel@gmail.com>
2016-06-22 04:52:48 -0700

    Added application source

---
+++ WASD Vision/KeyboardMouseHooks/MouseVision.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace KeyboardMouseHooks
+{
+    partial class MouseVision
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MouseImage = new System.Windows.Forms.PictureBox();
+            this.Grid = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MouseImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MouseImage
+            // 
+            this.MouseImage.BackColor = System.Drawing.Color.Transparent;
+            this.MouseImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MouseImage.Image = global::KeyboardMouseHooks.Properties.Resources.Mouse_Default;
+            this.MouseImage.InitialImage = null;
+            this.MouseImage.Location = new System.Drawing.Point(0, 0);
+            this.MouseImage.Name = "MouseImage";
+            this.MouseImage.Size = new System.Drawing.Size(597, 615);
+            this.MouseImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.MouseImage.TabIndex = 0;
+            this.MouseImage.TabStop = false;
+            // 
+            // Grid
+            // 
+            this.Grid.BackColor = System.Drawing.Color.Transparent;
+            this.Grid.Location = new System.Drawing.Point(-5000, -5000);
+            this.Grid.Name = "Grid";
+            this.Grid.Size = new System.Drawing.Size(10000, 10000);
+            this.Grid.TabIndex = 1;
+            this.Grid.TabStop = false;
+            this.Grid.Visible = false;
+            this.Grid.Click += new System.EventHandler(this.Grid_Click);
+            // 
+            // MouseVision
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(597, 615);
+            this.Controls.Add(this.MouseImage);
+            this.Controls.Add(this.Grid);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MouseVision";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "MouseVision";
+            this.TransparencyKey = System.Drawing.Color.Lime;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MouseVision_FormClosing);
+            this.Load += new System.EventHandler(this.MouseVision_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.MouseImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox MouseImage;
+        private System.Windows.Forms.PictureBox Grid;
+    }
+}
---
+++ WASD Vision/KeyboardMouseHooks/MouseVision.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KeyboardMouseHooks
+{
+    public partial class MouseVision : Form
+    {
+        public MouseVision()
+        {
+            InitializeComponent();
+            mouseHookCallbackSetup();
+
+            // move form to bottom right
+            PositionForm();
+        }
+
+        RamGecTools.MouseHook mouseHook = new RamGecTools.MouseHook();
+
+        private bool MouseLeftDown = false;
+        private bool MouseRightDown = false;
+        // private int GridPositionLeft = -5000;
+        // private int GridPositionTop = -5000;
+        // private int MousePositionDifferenceLeft = -1;
+        // private int MousePositionDifferenceTop = -1;
+
+        private void PositionForm()
+        {
+            Rectangle workingArea = Screen.GetWorkingArea(this);
+            Location = new Point(workingArea.Right - Size.Width,
+                                 workingArea.Bottom - Size.Height);
+        }
+
+        private void mouseHookCallbackSetup()
+        {
+            // register events
+            mouseHook.LeftButtonDown += new RamGecTools.MouseHook.MouseHookCallback(mouseHook_LeftButtonDown);
+            mouseHook.LeftButtonUp += new RamGecTools.MouseHook.MouseHookCallback(mouseHook_LeftButtonUp);
+            mouseHook.RightButtonDown += new RamGecTools.MouseHook.MouseHookCallback(mouseHook_RightButtonDown);
+            mouseHook.RightButtonUp += new RamGecTools.MouseHook.MouseHookCallback(mouseHook_RightButtonUp);
+
+            mouseHook.Install();
+        }
+
+        private void mouseHook_LeftButtonDown(RamGecTools.MouseHook.MSLLHOOKSTRUCT mouseStruct)
+        {
+            MouseLeftDown = true;
+
+            if(MouseRightDown)
+            {
+                MouseImage.Image = Properties.Resources.Mouse_Left_Right_Down;
+            }
+            else
+            {
+                MouseImage.Image = Properties.Resources.Mouse_Left_Down;
+            }
+        }
+
+        private void mouseHook_LeftButtonUp(RamGecTools.MouseHook.MSLLHOOKSTRUCT mouseStruct)
+        {
+            MouseLeftDown = false;
+
+            if (MouseRightDown)
+            {
+                MouseImage.Image = Properties.Resources.Mouse_Right_Down;
+            }
+            else
+            {
+                MouseImage.Image = Properties.Resources.Mouse_Default;
+            }
+        }
+
+        private void mouseHook_RightButtonDown(RamGecTools.MouseHook.MSLLHOOKSTRUCT mouseStruct)
+        {
+            MouseRightDown = true;
+
+            if (MouseLeftDown)
+            {
+                MouseImage.Image = Properties.Resources.Mouse_Left_Right_Down;
+            }
+            else
+            {
+                MouseImage.Image = Properties.Resources.Mouse_Right_Down;
+            }
+        }
+
+        private void mouseHook_RightButtonUp(RamGecTools.MouseHook.MSLLHOOKSTRUCT mouseStruct)
+        {
+            MouseRightDown = false;
+
+            if (MouseLeftDown)
+            {
+                MouseImage.Image = Properties.Resources.Mouse_Left_Down;
+            }
+            else
+            {
+                MouseImage.Image = Properties.Resources.Mouse_Default;
+            }
+        }
+
+        private void MouseVision_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        // 
+        // Grid movement under Mouse Vision form was too distracting
+        //
+        //void mouseHook_MouseMove(RamGecTools.MouseHook.MSLLHOOKSTRUCT mouseStruct)
+        //{
+        //    // set mouse position variables to initial mouse position once
+        //    if(MousePositionDifferenceLeft < 0)
+        //    {
+        //        MousePositionDifferenceLeft = mouseStruct.pt.x;
+        //        MousePositionDifferenceTop = mouseStruct.pt.y;
+        //    }
+
+        //    GridPositionLeft += (mouseStruct.pt.x - MousePositionDifferenceLeft);
+        //    GridPositionTop += (mouseStruct.pt.y - MousePositionDifferenceTop);
+
+        //    Grid.Location = new System.Drawing.Point(GridPositionLeft, GridPositionTop);
+
+        //    MousePositionDifferenceLeft = mouseStruct.pt.x;
+        //    MousePositionDifferenceTop = mouseStruct.pt.y;
+        //}
+
+
+        private void MouseVision_FormClosing(object sender, EventArgs e)
+        {
+            mouseHook.Uninstall();
+
+            Application.Exit();
+        }
+
+        private void Grid_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
